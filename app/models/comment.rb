ecb04877fbc9c6dbddd89100f29617acc0b7912e
github username <github email address>
2019-02-21 10:40:26 -0500

    polymorphic relationship seeded

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,3 @@
 class Comment < ApplicationRecord
-  belongs_to :guest
+  belongs_to :commentable, polymorphic: true
 end
--- app/serializers/creator_serializer.rb
+++ app/serializers/creator_serializer.rb
@@ -1,5 +1,7 @@
 class CreatorSerializer < ActiveModel::Serializer
   has_many :receptions
   has_many :guests, through: :receptions
+  has_many :photos, as: :imageable
+  has_many :comments, as: :commentable
   attributes :id, :name
 end
